e9b71c76b6aa55a96ea6756a6c41e82aa7b667fb
Brian Truong Nguyen <btn023@ucsd.edu>
2017-02-09 01:12:24 -0800

    FINISHED LAB 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	var name = req.query.name;
+    var desc = req.query.description;
+    var imgURL = 'http://lorempixel.com/400/400/people';
+    friend = (
+		{
+			"name": name,
+			"description": desc,
+			"imageURL": imgURL			
+		}
+    );
+    console.log(friend);
+    data.friends.push(friend);
  }
